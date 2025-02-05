1e7d170423259c3195c167b5a78fe6da9b4ef849
harshild185 <harshil.inventam@gmail.com>
2025-02-05 16:33:52 +0530

    Integrating TypeORM with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MyConfigModule } from './config/config.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MyConfigModule],
+  imports: [
+    MyConfigModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.DB_SYNC === 'true',
+      autoLoadEntities: true,
+      //Logger setings to log error's and warn's in the ORM.
+      logger: 'file',
+      logging: ['error'],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
