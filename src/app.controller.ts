f1b86e15cc25529e4a0fb149f80b744bca203263
harshild185 <harshil.inventam@gmail.com>
2025-02-06 10:24:53 +0530

    Build-in Logger use

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Ip, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Ip() Ip:string): string {
+    this.logger.log(`Request IP: ${Ip}`);
+    // second way 
+    Logger.verbose(`GetHello call from Ip: ${Ip}`,AppController.name);
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, 
+    // {// it will accept boolean or array of levels or custom logger service
+    //   logger: ['error', 'warn', 'debug'],
+    // }
+);
   const configService = app.get(ConfigService);
 
   // Retrieving the application port from the configuration or using the default value of 3000
   const port = configService.get<number>('app.port', 3000);
   
   await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
+
+  // direct
+  Logger.log(`This application is running on: ${await app.getUrl()}`)
+
+  //  With Logger context
+  const logger = new Logger("Bootstrap");
+  logger.debug(`Application is running on: ${await app.getUrl()}`);
+
+  // direct with context
+  Logger.warn(`Application is running on: ${await app.getUrl()}`, "Bootstrap");
 }
 bootstrap();
