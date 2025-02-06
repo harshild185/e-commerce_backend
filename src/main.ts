c99f3dd48e33599275ff6ce9842a17a1d76fb561
harshild185 <harshil.inventam@gmail.com>
2025-02-06 11:41:54 +0530

    customized build-in logger as a services

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { CustomLogger } from './config/custom.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, 
     // {// it will accept boolean or array of levels or custom logger service
     //   logger: ['error', 'warn', 'debug'],
     // }
-);
+  );
+  // overriding build-in logger
+  app.useLogger(new CustomLogger())
+
   const configService = app.get(ConfigService);
 
   // Retrieving the application port from the configuration or using the default value of 3000
@@ -26,5 +30,18 @@ async function bootstrap() {
 
   // direct with context
   Logger.warn(`Application is running on: ${await app.getUrl()}`, "Bootstrap");
+  Logger.error(`Application is running on: ${await app.getUrl()}`);
+
+  // Without overriding build-in logger
+  const customlogger = new CustomLogger('Bootstrap') // the string indicates the logLocation
+  customlogger.error(`Application is running on: ${await app.getUrl()}`);
+  customlogger.debug(`Application is running on: ${await app.getUrl()}`);
+  customlogger.warn(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+/**
+ * The major disadvantage of using this method is that we can’t 
+ * disable the logger at the application level. For example:
+ * 
+ * const app = await NestFactory.create(AppModule, { logger:false } );
+ */
